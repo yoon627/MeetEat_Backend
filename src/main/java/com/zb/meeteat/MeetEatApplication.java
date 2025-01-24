adc93021c4f63a835944a67f6b98f9220cc46dd9
yoon <science627@naver.com>
2025-01-24 18:46:49 +0900

    Initial commit

---
+++ src/main/java/com/zb/meeteat/MeetEatApplication.java
@@ -0,0 +1,13 @@
+package com.zb.meeteat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetEatApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MeetEatApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/zb/meeteat/MeetEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zb.meeteat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetEatApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
