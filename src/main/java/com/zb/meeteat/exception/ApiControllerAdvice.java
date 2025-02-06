6f3ec7453faa6e371972107e2a7df8a5f4fd6e2d
G1 <lovyellow923@naver.com>
2025-02-06 21:58:35 +0900

    feat: 요청 데이터 검증 오류 처리를 위한 글로벌 예외 핸들러 추가
    - @RestControllerAdvice를 활용한 ApiControllerAdvice 클래스 구현
    - MethodArgumentNotValidException 처리 로직 추가
    - 필드별 오류 메시지 매핑 및 로깅 처리

---
+++ src/main/java/com/zb/meeteat/exception/ApiControllerAdvice.java
@@ -0,0 +1,36 @@
+package com.zb.meeteat.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiControllerAdvice {
+
+  private static final Logger logger = LoggerFactory.getLogger(ApiControllerAdvice.class);
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public Map<String, String> handleValidationExceptions(
+      MethodArgumentNotValidException ex) {
+
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach((error) -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+      logger.error("[handleValidationExceptions]: {} - {}", fieldName, errorMessage);
+    });
+
+    return errors;
+  }
+}
