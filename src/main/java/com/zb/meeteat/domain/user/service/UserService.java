89e02932fc61c74b6331f8b621a4c9b20acd0716
Jeon Chan Min <chanmin010702@gmail.com>
2025-02-08 18:41:26 +0900

    Refactor(service): 회원가입 서비스 응답 타입 변경 및 예외 처리 수정 - ApiResponse<SignupResponseDto> 반환 방식을 User 반환 방식으로 변경 - 회원가입 성공 시, 저장된 User 엔티티를 반환하도록 수정 - 기존 CustomException을 IllegalArgumentException으로 변경하여 예외 처리 단순화

--- src/main/java/com/zb/meeteat/domain/user/service/UserService.java
+++ src/main/java/com/zb/meeteat/domain/user/service/UserService.java
@@ -1,14 +1,10 @@
 package com.zb.meeteat.domain.user.service;
 
-import com.zb.meeteat.domain.user.dto.ApiResponse;
 import com.zb.meeteat.domain.user.dto.SignupRequestDto;
-import com.zb.meeteat.domain.user.dto.SignupResponseDto;
 import com.zb.meeteat.domain.user.entity.Role;
 import com.zb.meeteat.domain.user.entity.SignUpType;
 import com.zb.meeteat.domain.user.entity.User;
 import com.zb.meeteat.domain.user.repository.UserRepository;
-import com.zb.meeteat.global.exception.CustomException;
-import com.zb.meeteat.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -22,7 +18,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public ApiResponse<SignupResponseDto> signup(SignupRequestDto requestDto) {
+    public User signup(SignupRequestDto requestDto) {
         validateDuplicateUser(requestDto.getEmail(), requestDto.getNickname());
 
         String encodedPassword = passwordEncoder.encode(requestDto.getPassword());
@@ -35,18 +31,15 @@ public class UserService {
                 .signupType(SignUpType.EMAIL)
                 .build();
 
-        userRepository.save(user);
-
-        return ApiResponse.success(new SignupResponseDto(user.getEmail(), user.getNickname())
-                , "회원가입이 성공적으로 완료되었습니다.");
+        return userRepository.save(user);
     }
 
     private void validateDuplicateUser(String email, String nickname) {
         if (userRepository.existsByEmail(email)) {
-            throw new CustomException(ErrorCode.EMAIL_ALREADY_REGISTERED);
+            throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
         }
         if (userRepository.existsByNickname(nickname)) {
-            throw new CustomException(ErrorCode.NICKNAME_ALREADY_REGISTERED);
+            throw new IllegalArgumentException("이미 사용 중인 닉네임입니다.");
         }
     }
 
