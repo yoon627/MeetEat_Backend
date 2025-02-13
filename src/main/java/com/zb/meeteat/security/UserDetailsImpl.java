f1ea8654112165468ca458d2d518f85f4f49651c
Jeon Chan Min <chanmin010702@gmail.com>
2025-02-14 00:37:04 +0900

    feat(Security): UserDetails 구현 클래스 추가
    - User 엔티티를 기반으로 Spring Security의 UserDetails 구현
    - getAuthorities(): 사용자의 권한(Role) 반환
    - getPassword(), getUsername(): 사용자 비밀번호 및 이메일 반환
    - getUser(): User 엔티티 직접 반환 메서드 추가

---
+++ src/main/java/com/zb/meeteat/security/UserDetailsImpl.java
@@ -0,0 +1,35 @@
+package com.zb.meeteat.security;
+
+import com.zb.meeteat.domain.user.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class UserDetailsImpl implements UserDetails {
+    private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(() -> user.getRole().name());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
