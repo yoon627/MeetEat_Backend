c5f1af866f1e1aa2b1596ee51ee5ae472b561bfe
Jeon Chan Min <chanmin010702@gmail.com>
2025-02-14 17:13:27 +0900

    refactor: tab 크기 2로 변경

--- src/main/java/com/zb/meeteat/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zb/meeteat/exception/GlobalExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.Map;
 
+
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
