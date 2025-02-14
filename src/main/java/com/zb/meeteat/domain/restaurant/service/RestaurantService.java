6a8b39a6bce25a69e4c51888f68efdef9d17914e
Jeon Chan Min <chanmin010702@gmail.com>
2025-02-14 16:51:10 +0900

    conflict 해결

--- src/main/java/com/zb/meeteat/domain/restaurant/service/RestaurantService.java
+++ src/main/java/com/zb/meeteat/domain/restaurant/service/RestaurantService.java
@@ -12,13 +12,9 @@ import com.zb.meeteat.domain.restaurant.repository.RestaurantRepository;
 import com.zb.meeteat.domain.restaurant.repository.RestaurantReviewRepository;
 import com.zb.meeteat.domain.user.entity.User;
 import com.zb.meeteat.domain.user.repository.UserRepository;
-import com.zb.meeteat.exception.UserCustomException;
+import com.zb.meeteat.exception.CustomException;
 import com.zb.meeteat.exception.ErrorCode;
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import com.zb.meeteat.exception.UserCustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -27,139 +23,145 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class RestaurantService {
 
-  private final S3ImageUpload s3ImageUpload;
-  private final RestaurantRepository restaurantRepository;
-  private final RestaurantReviewRepository restaurantReviewRepository;
-  private final UserRepository userRepository;
-  private final MatchingHistoryRepository matchingHistoryRepository;
-  private static int MAX_FILE_COUNT = 7;
-  private static int AFTER_MATCHING_TIME = 2;
+    private final S3ImageUpload s3ImageUpload;
+    private final RestaurantRepository restaurantRepository;
+    private final RestaurantReviewRepository restaurantReviewRepository;
+    private final UserRepository userRepository;
+    private final MatchingHistoryRepository matchingHistoryRepository;
+    private static int MAX_FILE_COUNT = 7;
+    private static int AFTER_MATCHING_TIME = 2;
 
-  public Page<RestaurantResponse> getRestaurantList(SearchRequest search) {
+    public Page<RestaurantResponse> getRestaurantList(SearchRequest search) {
 
-    String categoryName = search.getCategoryName().equals("전체") ? "" : search.getCategoryName();
-    search.setCategoryName(categoryName);
+        String categoryName = search.getCategoryName().equals("전체") ? "" : search.getCategoryName();
+        search.setCategoryName(categoryName);
 
-    // Pageable 객체 생성 (페이지, 사이즈, 정렬 방식)
-    Pageable pageable = PageRequest.of(search.getPage(), search.getSize());
+        // Pageable 객체 생성 (페이지, 사이즈, 정렬 방식)
+        Pageable pageable = PageRequest.of(search.getPage(), search.getSize());
 
-    // 정렬 기준에 따라 쿼리 메소드 실행
-    Page<RestaurantResponse> restaurants = Page.empty();
+        // 정렬 기준에 따라 쿼리 메소드 실행
+        Page<RestaurantResponse> restaurants = Page.empty();
 
-    if ("RATING".equals(search.getSorted())) {
-      restaurants = restaurantRepository.getRestaurantByRegionAndPlaceNameAndCategoryNameOrderByRatingDesc(
-          search.getRegion(),
-          search.getPlaceName(),
-          search.getCategoryName(),
-          pageable);
-    } else if ("DISTANCE".equals(search.getSorted())) {
-      if (Double.isNaN(search.getUserX()) || Double.isNaN(search.getUserX())) {
-        throw new UserCustomException(ErrorCode.USER_LOCATION_NOT_PROVIDED);
-      }
+        if ("RATING".equals(search.getSorted())) {
+            restaurants = restaurantRepository.getRestaurantByRegionAndPlaceNameAndCategoryNameOrderByRatingDesc(
+                    search.getRegion(),
+                    search.getPlaceName(),
+                    search.getCategoryName(),
+                    pageable);
+        } else if ("DISTANCE".equals(search.getSorted())) {
+            if (Double.isNaN(search.getUserX()) || Double.isNaN(search.getUserX())) {
+                throw new CustomException(ErrorCode.USER_LOCATION_NOT_PROVIDED);
+            }
 
-      restaurants = restaurantRepository.getRestaurantByRegionAndPlaceNameAndCategoryNameOrderByDistance(
-          search.getUserY(),
-          search.getUserX(),
-          search.getRegion(),
-          search.getPlaceName(),
-          search.getCategoryName(),
-          pageable);
-    } else {
-      restaurants = restaurantRepository.getRestaurantByRegionAndCategoryNameAndPlaceName(
-          search.getRegion(),
-          search.getPlaceName(),
-          search.getCategoryName(),
-          pageable);
+            restaurants = restaurantRepository.getRestaurantByRegionAndPlaceNameAndCategoryNameOrderByDistance(
+                    search.getUserY(),
+                    search.getUserX(),
+                    search.getRegion(),
+                    search.getPlaceName(),
+                    search.getCategoryName(),
+                    pageable);
+        } else {
+            restaurants = restaurantRepository.getRestaurantByRegionAndCategoryNameAndPlaceName(
+                    search.getRegion(),
+                    search.getPlaceName(),
+                    search.getCategoryName(),
+                    pageable);
+        }
+
+        return restaurants;
     }
 
-    return restaurants;
-  }
-
-  public RestaurantResponse getRestaurant(Long restaurantId) {
-    return restaurantRepository.getRestaurantById(restaurantId);
-  }
-
-  public Page<RestaurantReview> getRestaurantReviews(
-      Long restaurantId, int page, int size) {
-
-    // Pageable 객체 생성 (페이지, 사이즈, 정렬 방식)
-    Pageable pageable = PageRequest.of(page, size);
-
-    return restaurantReviewRepository.getRestaurantReviewByRestaurantId(restaurantId, pageable);
-  }
-
-  public RestaurantReview createReview(Long userId, CreateReviewRequest req) throws UserCustomException {
-
-    // 1. user 정보 가져오기
-    User user = userRepository.findById(userId)
-        .orElseThrow(() -> new UserCustomException(ErrorCode.BAD_REQUEST));
-
-    // 2. 매칭 참석 여부 확인
-    MatchingHistory history = matchingHistoryRepository.findById(req.getMatchingHistoryId())
-        .orElseThrow(() -> new UserCustomException(ErrorCode.BAD_REQUEST));
-
-    Matching matching = history.getMatching();
-    if (matching.getStatus().equals(MatchingStatus.CANCELED)
-        || history.getStatus().equals(MatchingStatus.CANCELED)) {
-      throw new UserCustomException(ErrorCode.REVIEW_NOT_ALLOWED_FOR_CANCELED_MATCHING);
+    public RestaurantResponse getRestaurant(Long restaurantId) {
+        return restaurantRepository.getRestaurantById(restaurantId);
     }
 
-    // 3. 후기 작성 시간 확인 (매칭 약속시간 이후 2시간)
-    LocalDateTime matchingTime = matching.getCreatedAt();
-    LocalDateTime currentTime = LocalDateTime.now();
-    Duration duration = Duration.between(matchingTime, currentTime);
-    if (duration.toHours() < AFTER_MATCHING_TIME) {
-      throw new UserCustomException(ErrorCode.REVIEW_TIME_NOT_EXCEEDED);
+    public Page<RestaurantReview> getRestaurantReviews(
+            Long restaurantId, int page, int size) {
+
+        // Pageable 객체 생성 (페이지, 사이즈, 정렬 방식)
+        Pageable pageable = PageRequest.of(page, size);
+
+        return restaurantReviewRepository.getRestaurantReviewByRestaurantId(restaurantId, pageable);
     }
 
-    // 4. 첨부파일 확인
-    String imageUrls = saveImage(req.getFiles());
+    public RestaurantReview createReview(Long userId, CreateReviewRequest req) throws UserCustomException {
 
-    // 5. 데이터 저장하기
-    RestaurantReview review = RestaurantReview.builder()
-        .rating(req.getRating())
-        .description(req.getDescription())
-        .imgUrl(imageUrls)
-        .matchingHistoryId(req.getMatchingHistoryId())
-        .user(user)
-        .restaurant(matching.getRestaurant())
-        .build();
+        // 1. user 정보 가져오기
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.BAD_REQUEST));
 
-    return restaurantReviewRepository.save(review);
-  }
+        // 2. 매칭 참석 여부 확인
+        MatchingHistory history = matchingHistoryRepository.findById(req.getMatchingHistoryId())
+                .orElseThrow(() -> new CustomException(ErrorCode.BAD_REQUEST));
 
-  public RestaurantReview getMyReviewByMatching(Long matchingHistoryId) {
-    MatchingHistory history = matchingHistoryRepository.findById(matchingHistoryId)
-        .orElseThrow(() -> new UserCustomException(ErrorCode.BAD_REQUEST));
+        Matching matching = history.getMatching();
+        if (matching.getStatus().equals(MatchingStatus.CANCELED)
+                || history.getStatus().equals(MatchingStatus.CANCELED)) {
+            throw new CustomException(ErrorCode.REVIEW_NOT_ALLOWED_FOR_CANCELED_MATCHING);
+        }
 
-    Matching matching = history.getMatching();
-    if (history.getStatus().equals(MatchingStatus.CANCELED) ||
-        matching.getStatus().equals(MatchingStatus.CANCELED)) {
-      throw new UserCustomException(ErrorCode.CANCELED_MATCHING);
+        // 3. 후기 작성 시간 확인 (매칭 약속시간 이후 2시간)
+        LocalDateTime matchingTime = matching.getCreatedAt();
+        LocalDateTime currentTime = LocalDateTime.now();
+        Duration duration = Duration.between(matchingTime, currentTime);
+        if (duration.toHours() < AFTER_MATCHING_TIME) {
+            throw new CustomException(ErrorCode.REVIEW_TIME_NOT_EXCEEDED);
+        }
+
+        // 4. 첨부파일 확인
+        String imageUrls = saveImage(req.getFiles());
+
+        // 5. 데이터 저장하기
+        RestaurantReview review = RestaurantReview.builder()
+                .rating(req.getRating())
+                .description(req.getDescription())
+                .imgUrl(imageUrls)
+                .matchingHistoryId(req.getMatchingHistoryId())
+                .user(user)
+                .restaurant(matching.getRestaurant())
+                .build();
+
+        return restaurantReviewRepository.save(review);
     }
 
-    return restaurantReviewRepository.findRestaurantReviewByMatchingHistoryId(matchingHistoryId);
-  }
+    public RestaurantReview getMyReviewByMatching(Long matchingHistoryId) {
+        MatchingHistory history = matchingHistoryRepository.findById(matchingHistoryId)
+                .orElseThrow(() -> new CustomException(ErrorCode.BAD_REQUEST));
 
-  private String saveImage(MultipartFile[] files) {
-    if (files.length > MAX_FILE_COUNT || files.length < 1
-        || files[0].isEmpty() || Objects.requireNonNull(files[0].getOriginalFilename()).isEmpty()) {
-      return null;
+        Matching matching = history.getMatching();
+        if (history.getStatus().equals(MatchingStatus.CANCELED) ||
+                matching.getStatus().equals(MatchingStatus.CANCELED)) {
+            throw new CustomException(ErrorCode.CANCELED_MATCHING);
+        }
+
+        return restaurantReviewRepository.findRestaurantReviewByMatchingHistoryId(matchingHistoryId);
     }
 
-    List<String> imgUrlList = new ArrayList<String>();
-    for (MultipartFile image : files) {
-      String newFileName = s3ImageUpload.uploadImage(image);
-      imgUrlList.add(newFileName);
-      log.info("➡️ 이미지 저장 :{} - {}", image.getOriginalFilename(), newFileName);
-    }
+    private String saveImage(MultipartFile[] files) {
+        if (files.length > MAX_FILE_COUNT || files.length < 1
+                || files[0].isEmpty() || Objects.requireNonNull(files[0].getOriginalFilename()).isEmpty()) {
+            return null;
+        }
 
-    return String.join(",", imgUrlList);
-  }
+        List<String> imgUrlList = new ArrayList<String>();
+        for (MultipartFile image : files) {
+            String newFileName = s3ImageUpload.uploadImage(image);
+            imgUrlList.add(newFileName);
+            log.info("➡️ 이미지 저장 :{} - {}", image.getOriginalFilename(), newFileName);
+        }
+
+        return String.join(",", imgUrlList);
+    }
 
 }
