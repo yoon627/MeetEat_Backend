4786c6f91c828b9371df63b0026f1a32c03673a2
yoon <science627@naver.com>
2025-02-16 23:17:05 +0900

    fix: security config cors origin 추가

--- src/main/java/com/zb/meeteat/config/SecurityConfig.java
+++ src/main/java/com/zb/meeteat/config/SecurityConfig.java
@@ -48,11 +48,14 @@ public class SecurityConfig {
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(authorize -> authorize
             .requestMatchers(HttpMethod.POST, "/api/users/signup", "/api/users/signin",
-                "/api/users/signin/*", "api/restaurants/search","api/restaurants/{restaurantId}").permitAll()
+                "/api/users/signin/*", "api/restaurants/search", "api/restaurants/{restaurantId}")
+            .permitAll()
             .requestMatchers(HttpMethod.POST, "/api/users/change-password", "/api/matching/request",
                 "/api/matching/join", "/api/matching/cancel")
             .authenticated() // 인증 필요
-            .requestMatchers(HttpMethod.GET, "/api/sse/subscribe", "/api/restaurants/{restaurantId}", "/api/restaurants/{restaurantId}/reviews").authenticated()
+            .requestMatchers(HttpMethod.GET, "/api/sse/subscribe",
+                "/api/restaurants/{restaurantId}", "/api/restaurants/{restaurantId}/reviews")
+            .authenticated()
             .anyRequest().authenticated()
         )
         .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class); // JWT 필터 추가
@@ -74,6 +77,8 @@ public class SecurityConfig {
         "http://localhost:5173/**"); // 명확한 Origin 명시
     corsConfigurationV1.addAllowedOriginPattern(
         "https://meet--eat.com"); // 명확한 Origin 명시
+    corsConfigurationV1.addAllowedOriginPattern(
+        "https://meet--eat.com/**"); // 명확한 Origin 명시
     corsConfigurationV1.addAllowedMethod("*");
     corsConfigurationV1.addAllowedHeader("*");
 
