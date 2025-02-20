a8188a9d4e6940001a3e62400fcdeabca29f8dd4
Jeon Chan Min <chanmin010702@gmail.com>
2025-02-21 04:33:47 +0900

    chore(config): CorsConfig 및 SecurityConfig 수정

--- src/main/java/com/zb/meeteat/config/CorsConfig.java
+++ src/main/java/com/zb/meeteat/config/CorsConfig.java
@@ -11,20 +11,43 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+//  @Bean
+//  public CorsFilter corsFilter() {
+//    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//    CorsConfiguration config = new CorsConfiguration();
+//
+//    // 모든 출처에서 요청 허용 (보안상 운영에서는 특정 출처만 허용해야 함)
+//    config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "https://meet--eat.com"));
+//
+//
+//    // 모든 HTTP 메서드 허용 (GET, POST, PUT, DELETE 등)
+//    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH" ));
+//
+//    // 허용할 헤더를 특정 값으로 제한
+//    config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+//
+//    // 자격 증명 허용 (Authorization 헤더 포함)
+//    config.setAllowCredentials(true);
+//
+//    // 모든 경로에 대해 설정 적용
+//    source.registerCorsConfiguration("/**", config);
+//
+//    return new CorsFilter(source);
+//  }
+
   @Bean
   public CorsFilter corsFilter() {
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     CorsConfiguration config = new CorsConfiguration();
 
-    // 모든 출처에서 요청 허용 (보안상 운영에서는 특정 출처만 허용해야 함)
-    config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "https://meet--eat.com"));
+    // 모든 Origin 허용 (테스트용)
+    config.setAllowedOriginPatterns(List.of("*"));
 
-
-    // 모든 HTTP 메서드 허용 (GET, POST, PUT, DELETE 등)
+    // 모든 HTTP 메서드 허용
     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH" ));
 
-    // 허용할 헤더를 특정 값으로 제한
-    config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+    // 모든 헤더 허용
+    config.setAllowedHeaders(List.of("*"));
 
     // 자격 증명 허용 (Authorization 헤더 포함)
     config.setAllowCredentials(true);
@@ -34,4 +57,5 @@ public class CorsConfig {
 
     return new CorsFilter(source);
   }
+
 }
